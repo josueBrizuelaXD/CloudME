209c59cf2362d9a47c8445866fbd9e3bf31eac39
Josh <Josh@joshs-macbook-pro.local>
2016-09-17 18:03:10 -0500

    first draft of the auth screen

---
+++ SocialMedia/AuthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SocialMedia
+//
+//  Created by Josh on 9/16/16.
+//  Copyright © 2016 Josh. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+   
+    @IBOutlet weak var bgVisualView: UIVisualEffectView!
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bgVisualView.layer.cornerRadius = 20.0
+        bgVisualView.clipsToBounds = true
+            }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+    }
+
+}
+
---
+++ SocialMedia/RoundView.swift
@@ -0,0 +1,15 @@
+//
+//  RoundView.swift
+//  SocialMedia
+//
+//  Created by Josh on 9/17/16.
+//  Copyright © 2016 Josh. All rights reserved.
+//
+
+import UIKit
+
+class RoundView: UIView {
+
+   
+
+}
